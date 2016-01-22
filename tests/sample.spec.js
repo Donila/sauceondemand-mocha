f5f7d399b40ec366a05260a6f827b80ad14f9a9a
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2016-01-22 20:38:16 +0300

    initial commit

---
+++ tests/sample.spec.js
@@ -0,0 +1,86 @@
+/* globals require*/
+
+var assert = require('assert'),
+    test = require('selenium-webdriver/testing'),
+    webdriver = require('selenium-webdriver'),
+    SauceLabs = require('saucelabs'),
+    username = process.env.SAUCE_USERNAME,
+    accessKey = process.env.SAUCE_ACCESS_KEY,
+    saucelabs = new SauceLabs({
+        username: username,
+        password: accessKey
+    });
+
+var endOfLine = require('os').EOL;
+
+test.describe('Google Search', function() {
+    this.timeout(60000);
+
+    var driver;
+
+    test.beforeEach(function() {
+        var browser = process.env.BROWSER,
+            version = process.env.VERSION,
+            platform = process.env.PLATFORM,
+            server = 'http://' + username + ':' + accessKey +
+                '@ondemand.saucelabs.com:80/wd/hub';
+
+        driver = new webdriver.Builder().
+            withCapabilities({
+                'browserName': browser,
+                'platform': platform,
+                'version': version,
+                'username': username,
+                'accessKey': accessKey
+            }).
+            usingServer(server).
+            build();
+
+        driver.getSession().then(function (sessionid){
+            driver.sessionID = sessionid.id_;
+        });
+
+    });
+
+    test.afterEach(function(done) {
+        var title = this.currentTest.title,
+            passed = (this.currentTest.state === 'passed') ? true : false;
+
+        var onDemandOutputString = 'SauceOnDemandSessionID=' + driver.sessionID + ' job-name=' + this.currentTest.title + endOfLine;
+
+        console.log(onDemandOutputString);
+
+        saucelabs.updateJob(driver.sessionID, {
+            name: title,
+            passed: passed
+        }, done);
+
+        driver.quit();
+    });
+
+    /*test.after(function(done) {
+        var onDemandOutputString = 'SauceOnDemandSessionID=' + driver.sessionID + ' job-name=' + this.currentTest.parent.title;
+
+        console.log(onDemandOutputString);
+
+        done();
+    });*/
+
+    test.it('get google title', function() {
+        driver.get('http://google.com');
+
+        driver.getTitle().then(function(value) {
+            assert.equal(value, 'Google');
+        });
+    });
+
+    test.it('searching for webdriver using google', function() {
+        driver.get('http://google.com');
+
+        var searchBox = driver.findElement(webdriver.By.name('q'));
+        searchBox.sendKeys('webdriver');
+        searchBox.getAttribute('value').then(function(value) {
+            assert.equal(value, 'webdriver');
+        });
+    });
+});
