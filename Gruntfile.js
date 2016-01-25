b246142da01d285741776177de1c66ae945e2ab9
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2016-01-25 13:19:45 +0300

    Added default browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,27 +3,31 @@
 
 module.exports = function (grunt) {
 
-    var browsers = process.env.SAUCE_ONDEMAND_BROWSERS;
-    /*var browsers = [
+    var defaultBrowsers = [
         {
-            "os":"Linux",
-            "platform":"LINUX",
-            "browser":"chrome",
-            "browser-version":"47",
-            "long-name":"Google Chrome",
-            "long-version":"47.0.2526.73",
-            "url":"sauce-ondemand:?os=Linux&browser=chrome&browser-version=47&username=daniildziaruhin&access-key=5ff41c7f-e213-46d1-82a9-3ef39194ddca"
+            "os": "Linux",
+            "platform": "LINUX",
+            "browser": "chrome",
+            "browser-version": "47",
+            "long-name": "Google Chrome",
+            "long-version": "47.0.2526.73",
+            "url": "sauce-ondemand:?os=Linux&browser=chrome&browser-version=47&username=daniildziaruhin&access-key=5ff41c7f-e213-46d1-82a9-3ef39194ddca"
         },
         {
-            "os":"Windows 10",
-            "platform":"XP",
-            "browser":"firefox",
-            "browser-version":"43",
-            "long-name":"Firefox",
-            "long-version":"43.0.",
-            "url":"sauce-ondemand:?os=Windows 10&browser=firefox&browser-version=43&username=daniildziaruhin&access-key=5ff41c7f-e213-46d1-82a9-3ef39194ddca"
+            "os": "Windows 10",
+            "platform": "XP",
+            "browser": "firefox",
+            "browser-version": "43",
+            "long-name": "Firefox",
+            "long-version": "43.0.",
+            "url": "sauce-ondemand:?os=Windows 10&browser=firefox&browser-version=43&username=daniildziaruhin&access-key=5ff41c7f-e213-46d1-82a9-3ef39194ddca"
         }
-    ];*/
+    ];
+
+    var browsers = process.env.SAUCE_ONDEMAND_BROWSERS || defaultBrowsers;
+
+    console.log('browsers: ');
+    console.log(JSON.stringify(browsers));
 
     // configure tasks
     var browserTasks = [];
@@ -37,7 +41,7 @@ module.exports = function (grunt) {
         shell: {
             runTests: {
                 command: function(platform, browser, version) {
-                    return 'PLATFORM='+platform+' BROWSER='+browser+' VERSION='+version+' ./node_modules/.bin/_mocha tests -R mocha-junit-reporter'
+                    return 'PLATFORM='+platform+' BROWSER='+browser+' VERSION='+version+' ./node_modules/.bin/mocha tests -R mocha-junit-reporter'
                 }
             }
         },
